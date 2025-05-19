0d9c0f3e9effc2f8c3746c69b48be169aa577355
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-19 15:44:06 -0600

    feat: uso de callbacks con el método sort ()

--- js-04-arrays_and_loops/src/js/main.js
+++ js-04-arrays_and_loops/src/js/main.js
@@ -300,3 +300,31 @@ const generarNumerosDeLaSuerte = (size = 6, minNum = 1, maxNum = 54) => {
 document.getElementById("generarnumbut").addEventListener("click", () => {
     generarNumerosDeLaSuerte();
 });
+
+// -----------Uso del método sort --------------
+
+const numerosIniciales = [ 5, 33, 8, 100, 4, 2, 7, 6 ];
+             //[ 5, 33, 8, 100, 4, 2, 7, 6]  iteración 0
+             //[ 5, 8, 33, 100, 4, 2, 7, 6]  iteración 1
+             //[ 5, 33, 8, 100, 4, 2, 7, 6]  iteración 2
+             //[ 5, 33, 8, 4, 100, 2, 7, 6]  iteración 3
+             //[ 5, 33, 8, 4, 2, 100, 7, 6]  iteración 4
+             //[ 5, 33, 8, 4, 2, 7, 100, 6]  iteración 5
+             //[ 5, 33, 8, 4, 2, 7, 6, 100]  iteración 6
+
+
+const comparaNumeros = (a,b) => {
+    if ( a < b ) return -1;
+    if ( a > b ) return 1;
+    return 0;
+
+}
+
+const ordenarNumeros = ( numerosDesordenados, fncCallBack )=>{
+    const numerosOrdenados = numerosDesordenados;
+    numerosOrdenados.sort(fncCallBack);
+    return numerosOrdenados;
+}
+
+console.log (numerosIniciales);
+console.log (ordenarNumeros(numerosIniciales, comparaNumeros));
