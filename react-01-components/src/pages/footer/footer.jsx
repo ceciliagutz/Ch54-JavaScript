37ff93b570d09ce1714654aec84422b8c5c899ae
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-29 14:37:33 -0600

    "feat:Creación del footer"

---
+++ react-01-components/src/pages/footer/footer.jsx
@@ -0,0 +1,15 @@
+
+const Footer = () =>{
+
+    return (
+        <footer>
+            <p>Ch54 - 2025 </p>
+            <em> Es leviosa es leviosaá </em>
+        </footer>
+
+
+    );
+
+
+};
+export {Footer};
