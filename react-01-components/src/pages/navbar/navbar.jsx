69a14c09b89b7b0ad7e52cd87d16292196091f91
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-29 14:38:42 -0600

    "feat: creacion del navbar"

---
+++ react-01-components/src/pages/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+
+const NavBar = () =>{
+
+    return (
+        <nav>
+            <ul>
+                <li>Severus Snape</li>
+                <li>Ron Weasly</li>
+                <li>Sirius Black</li>
+            </ul>       
+     </nav>
+    );
+
+};
+
+export{ NavBar};
