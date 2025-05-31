5baccedc6d4818f7c9b88fae5af80f858e01d80f
Cecilia Gutierrez <ceciliagutierrezurbano@gmail.com>
2025-05-30 18:56:11 -0600

    "feat: Actualikzación app"

--- react-01-components/src/App.jsx
+++ react-01-components/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css'
 import { Footer } from './pages/footer/footer';
 import { NavBar } from './pages/navbar/navbar';
 import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
 
 function App() {
 
   const myJsx = (
   <>
-      <NavBar/>
-      <Home />
+      <NavBar title="Día de Harry Potter"/>
+      <Home  name="Cecilia Black" hogwartsHome="Gryffindor" />
+      <Home name="CeciSirius Black" hogwartsHome="Slytherin"/>
+       <Products/>
        <Footer/>  
   </>
  
